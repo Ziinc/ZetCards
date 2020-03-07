9dffa0c9f281295e5336a3848b6b920c89ffedf5
ziinc <tzeyiing@gmail.com>
2020-03-07 17:45:13 +0800

    added some logic, added elasticlunr

---
+++ zet/lib/core.ts
@@ -0,0 +1,64 @@
+import * as vscode from "vscode";
+import { isObject } from "util";
+const fs = require("fs");
+var elasticlunr = require("elasticlunr");
+const path = require("path");
+let index;
+export const buildSearchIndex = async () => {
+  if (index) return;
+  index = elasticlunr(function() {
+    this.setRef("id");
+    this.addField("title");
+    this.addField("body");
+  });
+  var doc1 = {
+    id: 1,
+    title: "Oracle released its latest database Oracle 12g",
+    body:
+      "Yestaday Oracle has released its new database Oracle 12g, this would make more money for this company and lead to a nice profit report of annual year."
+  };
+
+  var doc2 = {
+    id: 2,
+    title: "Oracle released its profit report of 2015",
+    body:
+      "As expected, Oracle released its profit report of 2015, during the good sales of database and hardware, Oracle's profit of 2015 reached 12.5 Billion."
+  };
+
+  index.addDoc(doc1);
+  index.addDoc(doc2);
+
+  //   index = FlexSearch.create();
+  const workspacePath = vscode.workspace.rootPath;
+
+  const files = await lsR(workspacePath).catch(console.error);
+  console.log("files", files);
+};
+export const searchFiles = async () => {
+  if (!index) console.log("index not defined");
+  const res = index.search("Oracle database profit");
+  console.log(res);
+};
+
+const ls = async (path: string) => {
+  return new Promise<string[]>((resolve, reject) => {
+    fs.readdir(path, (error, files) => {
+      if (error) reject(error);
+      resolve(files);
+    });
+  });
+};
+
+const lsR = async (targetPath: string) => {
+  let currFiles = await ls(targetPath);
+  console.log(currFiles);
+  for (let f of currFiles) {
+    const currFilePath = path.join(targetPath, f);
+    if (fs.lstatSync(currFilePath).isDirectory() == true) {
+      const newPath = path.join(targetPath, f);
+      const moreFiles = await lsR(newPath);
+      currFiles.concat(moreFiles);
+    }
+  }
+  return currFiles;
+};
--- zet/lib/extension.ts
+++ zet/lib/extension.ts
@@ -2,12 +2,39 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as core from "zet-core";
+import { buildSearchIndex, searchFiles } from "./core";
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// your extension is activated the very first time the command is execu[t]ed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "zet" is now active!');
+  buildSearchIndex();
+  const selector = "*";
+  const completionItemProviderDisposable = vscode.languages.registerCompletionItemProvider(
+    selector,
+    {
+      provideCompletionItems(document, position, token, context) {
+        const items = [];
+        searchFiles();
+        for (let tag of ["test", "testing123"]) {
+          items.push(new vscode.CompletionItem(tag));
+        }
+        console.log(items);
+
+        return items;
+      }
+    },
+    "[["
+  );
+
+  // const documentLinkProvider = new HashtagsDocumentLinkProvider();
+  // const documentLinkProviderDisposable = vscode.languages.registerDocumentLinkProvider(
+  //   selector,
+  //   documentLinkProvider
+  // );
+
+  context.subscriptions.push(completionItemProviderDisposable);
+  // context.subscriptions.push(documentLinkProviderDisposable);
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
@@ -17,6 +44,7 @@ export function activate(context: vscode.ExtensionContext) {
     () => {
       // The code you place here will be executed every time your command is executed
 
+      console.log("hello!");
       // Display a message box to the user
       vscode.window.showInformationMessage(`from rust: ${core.hello_world()}`);
     }
